f4d2b71ff0c2bd4eb10194a50eb7d28caa887b9d
Anoush Mistry <anoushmistry@gmail.com>
2025-08-24 18:12:53 +0530

    Abstract Base State class creacted along with a new StateManager class that handles state behaviour (start, update, collision and exit) and transitioning between states

---
+++ Procedural Animation in Unity/Assets/Scripts/StateMachine/StateManager.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateManager<EState> : MonoBehaviour where EState : Enum
+{
+    protected Dictionary<EState, BaseState<EState>> m_States = new Dictionary<EState, BaseState<EState>>();
+
+    protected BaseState<EState> CurrentState;
+
+    protected bool isTransitioning = false;
+
+    private void Start()
+    {
+        CurrentState.EnterState();
+    }
+
+    private void Update()
+    {
+        EState nextStateKey = CurrentState.GetNextState();
+
+        if (nextStateKey.Equals(CurrentState.StateKey))
+        {
+            CurrentState.UpdateState();
+        }
+        else if(!isTransitioning)
+        {
+            TransitionToState(CurrentState.StateKey);
+        }
+    }
+
+    private void TransitionToState(EState stateKey)
+    {
+        isTransitioning = true;
+        CurrentState.ExitState();
+        CurrentState = m_States[stateKey];
+        CurrentState.EnterState();
+        isTransitioning = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CurrentState.OnTriggerEnter(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        CurrentState.OnTriggerStay(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CurrentState.OnTriggerExit(other);
+    }
+}
